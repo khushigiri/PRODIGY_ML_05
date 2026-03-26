3b4a7736ebd2dd21a25374020e48c82e687a6ea3
khushigiri <02khushi.giri@gmail.com>
2026-03-26 21:30:27 +0530

    Removed Tensorflow

--- predict.py
+++ predict.py
@@ -1,10 +1,14 @@
 import numpy as np
 import pickle
-from keras.models import load_model   
+import tflite_runtime.interpreter as tflite
 from utils.preprocess import preprocess_image
 from utils.calorie_data import calories_dict
 
-model = load_model("model/food_model.h5", compile=False)
+interpreter = tflite.Interpreter(model_path="model/model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 with open("model/class_labels.pkl", "rb") as f:
     class_indices = pickle.load(f)
@@ -12,12 +16,15 @@ with open("model/class_labels.pkl", "rb") as f:
 class_labels = list(class_indices.keys())
 
 def predict_food(img_path):
-    img_array = preprocess_image(img_path)
+    img_array = preprocess_image(img_path).astype(np.float32)
 
-    prediction = model.predict(img_array)
+    interpreter.set_tensor(input_details[0]['index'], img_array)
+    interpreter.invoke()
+
+    prediction = interpreter.get_tensor(output_details[0]['index'])
     class_index = np.argmax(prediction)
 
     food = class_labels[class_index]
     calories = calories_dict.get(food, "Unknown")
 
-    return food, calories, prediction   
+    return food, calories, prediction
