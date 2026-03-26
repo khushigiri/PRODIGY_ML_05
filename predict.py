c6f6d180bd427b30adb2bf34b0bf23113859ecef
khushigiri <02khushi.giri@gmail.com>
2026-03-26 21:27:45 +0530

    Changed requirements

--- predict.py
+++ predict.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pickle
-from tensorflow.keras.models import load_model
+from keras.models import load_model   
 from utils.preprocess import preprocess_image
 from utils.calorie_data import calories_dict
 
-model = load_model("model/food_model.h5")
+model = load_model("model/food_model.h5", compile=False)
 
 with open("model/class_labels.pkl", "rb") as f:
     class_indices = pickle.load(f)
@@ -20,4 +20,4 @@ def predict_food(img_path):
     food = class_labels[class_index]
     calories = calories_dict.get(food, "Unknown")
 
-    return food, calories, 
+    return food, calories, prediction   
